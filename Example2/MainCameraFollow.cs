cce41c02ae76472824373c8fc3c9bfcd9f44e3d2
James Frowen <jamesfrowendev@gmail.com>
2022-01-05 17:15:34 +0000

    adding smoothing to camera

--- Example2/MainCameraFollow.cs
+++ Example2/MainCameraFollow.cs
@@ -7,6 +7,10 @@ namespace JamesFrowen.CSP
     {
         public Vector3 positionOffset;
         public Vector3 eularOffset;
+
+        [Range(0, 1)]
+        public float smooth = 0.2f;
+
         private Transform follower;
 
         private void Awake()
@@ -26,8 +30,8 @@ namespace JamesFrowen.CSP
             if (follower == null)
                 return;
 
-            Vector3 pos = transform.position + positionOffset;
-            follower.position = pos;
+            Vector3 target = transform.position + positionOffset;
+            follower.position = Vector3.Lerp(follower.position, target, smooth);
             //follower.rotation = Quaternion.Euler(eularOffset);
         }
     }
